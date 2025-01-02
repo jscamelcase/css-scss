222405e44521ed377d08dad225b43239f286134a
Carl Paraskevas <carl.paraskevas@gmail.com>
2025-01-02 18:20:08 +0000

    learning-journalsingle blog css

--- learning-journal/script.js
+++ learning-journal/script.js
@@ -3,3 +3,34 @@ const date = document.getElementById("copyright-date");
 //Applies the current date to the footer;
 const thisYear = new Date();
 date.textContent = thisYear.getFullYear();
+
+//Handle non-CSS responsiveness
+
+function handleWidthChange() {
+  const mq = window.matchMedia("(min-width: 768px)");
+
+  const singlePostElements = document.querySelectorAll(
+    ".single-post h1, .single-post time, .single-post p, .single-post h2"
+  );
+
+  const singlePostElementsArray = [...singlePostElements];
+
+  if (mq.matches) {
+    singlePostElementsArray.forEach((element) => {
+      element.style.paddingLeft = "5rem";
+      element.style.paddingRight = "5rem";
+    });
+  } else {
+    singlePostElementsArray.forEach((element) => {
+      console.log("hello");
+      element.style.paddingLeft = "2rem";
+      element.style.paddingRight = "2rem";
+    });
+  }
+}
+
+let resizeTimeout;
+window.addEventListener("resize", () => {
+  clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(handleWidthChange, 200);
+});
